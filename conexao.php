d93abb8e405f0e8502e7385ea4c2423d6d84cac1
heeenryy <carlos16171920@gmail.com>
2026-06-30 20:40:28 -0300

    atualizada as conexões do banco

--- conexao.php
+++ conexao.php
@@ -1,7 +1,7 @@
 <?php 
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
-    $conn = mysqli_connect("127.0.0.1","root","123456","cursosdb" );
+    $conn = mysqli_connect("sql204.byethost14.com","b14_42308781","Ch261956","b14_42308781_loja" );
     if (!$conn) {
         die("Não foi possivel conectar ao banco ". mysqli_connect_error());
     } else {
